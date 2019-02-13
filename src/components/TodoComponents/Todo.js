70ba58446e786cecfd05b670c6e3f9b9eb615e0e
Max Trestman <mtrestman1@android-4105f9b98c1d7f96.workspace.codeandsupply.co>
2019-02-13 15:42:11 -0500

    initial commit

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import TodoList from './TodoList';
+
+
+ class TodoList extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+          todoList: todoList,
+          task: '',
+          id: '',
+          completed: '',
+
+        };
+    }
+}
+
+
+
+
+
+
+
+export default TodoList;
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,41 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+
+
+
+const todoList = [
+    {
+      task: 'Make Breakfast',
+      id: 123,
+      completed: false
+    },
+    {
+      task: 'Workout',
+      id: 1234,
+      completed: false
+    },
+    {
+    task: 'Pack Lunch',
+    id: 12345,
+    completed: false
+  },
+  {
+    task: 'Complete Assignment',
+    id: 123456,
+    completed: false
+  },
+  {
+    task: 'Pick Up Dinner',
+    id: 1234567,
+    completed: false
+  },
+  ];
+
+ 
+
+ 
+
+
+
+  export default todoList;
